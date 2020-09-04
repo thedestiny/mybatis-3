a47812da263c0192c91e21d0d5f6417dd7e18d32
liangwenchao <liangwc@hxmec.com>
2020-09-04 10:45:36 +0800

    保存修改

--- src/test/java/org/apache/ibatis/type/usesjava8/InstantTypeHandlerTest.java
+++ src/test/java/org/apache/ibatis/type/usesjava8/InstantTypeHandlerTest.java
@@ -1,5 +1,5 @@
 /**
- *    Copyright 2009-2018 the original author or authors.
+ *    Copyright 2009-2019 the original author or authors.
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -13,20 +13,17 @@
  *    See the License for the specific language governing permissions and
  *    limitations under the License.
  */
-package org.apache.ibatis.type.usesjava8;
+package org.apache.ibatis.type;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 import java.sql.Timestamp;
 import java.time.Instant;
 
-import org.apache.ibatis.type.BaseTypeHandlerTest;
-import org.apache.ibatis.type.InstantTypeHandler;
-import org.apache.ibatis.type.TypeHandler;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-public class InstantTypeHandlerTest extends BaseTypeHandlerTest {
+class InstantTypeHandlerTest extends BaseTypeHandlerTest {
 
   private static final TypeHandler<Instant> TYPE_HANDLER = new InstantTypeHandler();
   private static final Instant INSTANT = Instant.now();
